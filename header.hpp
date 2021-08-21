0e7e0717729d7efe2b2bfecaebcf6e7b021790d6
88888812 <ndp2012@yandex.ru>
2021-08-21 16:54:08 +0300

    первые условия тестов и первые шаги proving

--- header.hpp
+++ header.hpp
@@ -2,4 +2,10 @@
 #ifndef AUTHENTICATION_PROTOCOL_HEADER_HPP
 #define AUTHENTICATION_PROTOCOL_HEADER_HPP
 #include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+struct  point {
+    int x=0,y=0;
+};
 #endif //AUTHENTICATION_PROTOCOL_HEADER_HPP
