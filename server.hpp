2ccf276cd1f542a94fa86814d4c96b60c90e4ee8
88888812 <ndp2012@yandex.ru>
2021-09-02 22:40:10 +0300

    add class client

--- server.hpp
+++ server.hpp
@@ -6,18 +6,19 @@
 void server()
 {
     Curve curve;
+    client A;
     std::vector<point>Y;
-    Y= client_gives_initial_points(curve);
+    Y= A.client_gives_initial_points(curve);
 
-    point R= clients_point(curve);
+    point R= A.clients_point(curve);
 
     //. Проверяющий B генерирует случайную битовую строку
     // ( a1 , ..., am)∈{0, 1}m и передает ее абоненту A.
     //через Бинарные литералы
-    int m= get_m();
+    int m= A.get_m();
     int bit_string; //длины m
 
-    ++++-++++++++++++++++int s = clients_summation(bit_string);
+    int s = A.clients_summation(bit_string);
 
 }
 #endif //AUTHENTICATION_PROTOCOL_SERVER_HPP
