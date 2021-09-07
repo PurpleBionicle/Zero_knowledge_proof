603a8e5357ed29c9d8e06f56748602cf72f11e38
88888812 <ndp2012@yandex.ru>
2021-09-08 00:21:03 +0300

    перебор заменен на алгоритм Евклида для нахождения обратных чисел

--- header.hpp
+++ header.hpp
@@ -6,8 +6,10 @@
 #include <string>
 #include <cmath>
 #include "curve.hpp"
+#include "point.hpp"
+#include <exception>
 #include "client.hpp"
 #include "server.hpp"
 double  a=2;// коэффициент кривой
-int p;
+int p=2;
 #endif //AUTHENTICATION_PROTOCOL_HEADER_HPP
--- point.hpp
+++ point.hpp
@@ -5,7 +5,8 @@
 #ifndef AUTHENTICATION_PROTOCOL_POINT_HPP
 #define AUTHENTICATION_PROTOCOL_POINT_HPP
 
-#include "header.hpp"
+
+#include "curve.hpp"
 
 int two_notation(int num) {
     //перевод из 10сс в 2сс
@@ -24,15 +25,39 @@ int two_notation(int num) {
 double a = 2;// коэффициент кривой
 int p;
 
-int inverse(int num) {
-    ///по модулю p всегда
 
-    if (num < 0) num += p;
-    for (int i = 0; i < p; ++i) {
-        if ((i * num) % p == 1) return i;
+int inverse(int mod, int num) {
+    if (num>=mod) {
+        throw std::invalid_argument("value is bigger than module");
+    }
+    int x = 1, y = 0;
+    int x1 = 0, y1 = 1;
+    int q;
+    int module =  mod;
+    int num_to_inverse =  num;
+    int i = 0;
+    while ( mod != 1 && num != 1) {
+        if ( mod > num)q =  mod / num;
+        else q = num /  mod;
+        if (i % 2 == 0) {
+            x = x - q * x1;
+            y = y - q * y1;
+            mod = x * module + y * num_to_inverse;
+        } else {
+            x1 = x1 - q * x;
+            y1 = y1 - q * y;
+            num = x1 * module + y1 * num_to_inverse;
+        }
+        ++i;
+    }
+    if ( mod == 1) {
+        if (y < 0)y += module;
+        return y;
+    } else {
+        if (y1 < 0)y1 += module;
+        return y1;
     }
 }
-
 
 class point {
 public:
@@ -56,9 +81,9 @@ public:
         point A;
         int alfa;
         if (*this == B) {
-            alfa = (3 * pow(B.x, 2) + a) * inverse(2 * B.y);
+            alfa = (3 * pow(B.x, 2) + a) * inverse(p,2 * B.y);
         } else {
-            alfa = (B.y - this->y) * inverse(B.x - this->x);
+            alfa = (B.y - this->y) * inverse(p,B.x - this->x);
         }
         A.x = (alfa * alfa - this->x - B.x) % p;
         A.y = (alfa * (this->x - A.x) - this->y) % p;
@@ -70,9 +95,9 @@ public:
     point operator+=(point &B) {
         int alfa;
         if (*this == B) {
-            alfa = (3 * pow(B.x, 2) + a) * inverse(2 * B.y);
+            alfa = (3 * pow(B.x, 2) + a) * inverse(p,2 * B.y);
         } else {
-            alfa = (B.y - this->y) * inverse(B.x - this->x);
+            alfa = (B.y - this->y) * inverse(p,B.x - this->x);
         }
         int xx = this->x;
         this->x = (alfa * alfa - this->x - B.x) % p;////Добавить по модулю p
