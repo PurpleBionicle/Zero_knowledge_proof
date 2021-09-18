9452047751074b231b5a8627b5e79ca6f7a643a7
88888812 <ndp2012@yandex.ru>
2021-09-18 20:59:47 +0300

    cmake для gtest и немного тестов

--- test.cpp
+++ test.cpp
@@ -1,10 +1,33 @@
 #include <gtest/gtest.h>
+#include "header.hpp"
 
-#include "main.cpp"
+TEST(Example, EmptyTest) {
+    EXPECT_TRUE(true);
+}
 
-TEST(1)
+TEST(points,constructors){
+    point a(2,3);
+    point b;
+    b.x=2;b.y=3;
+    EXPECT_EQ(2,a.x);
+    EXPECT_EQ(3,a.y);
+    EXPECT_EQ(2,b.x);
+    EXPECT_EQ(3,b.y);
+    EXPECT_EQ(a.y,b.y);
+    EXPECT_EQ(a,a);
+    EXPECT_EQ(a,b);
+}
+TEST(points,sum_multi)
 {
-    EXPECT_EQ(1,1);
+    point a(2,3);
+    point b;
+    b.x=2;b.y=3;
+    EXPECT_EQ(a+a,a*2);
+    EXPECT_EQ(a+a+a,a*3);
+    b+=a;
+    EXPECT_EQ(b,a+a);
+   //// EXPECT_EQ(a+a+a+a,a*4); //  бесконечный запуск ??????
+}
 // проверка на сингулярность  эллиптической кривой.
 
 //Проверка G∈эллиптическая кривая
@@ -16,4 +39,3 @@ TEST(1)
 // проверка, q – некоторый (достаточно большой) простой делитель числа |E p (a, b)|
 
 //Проверка что 1<xi<q-2
-}
