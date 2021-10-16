29b35c70b170e462f284202c34bffc6cfb4868a1
88888812 <ndp2012@yandex.ru>
2021-10-16 18:52:56 +0300

    add gmp library

--- main.cpp
+++ main.cpp
@@ -1,22 +1,15 @@
 
 #include "server.hpp"
+#include "header.hpp"
 int main() {
     //clients     //server
-   server();
+   // server();
+    mpz_class a, b, c;
 
+    a = 1234;
+    b = "-5678";
+    c = a*b;
+    std::cout << "sum is " << c << "\n";
+    std::cout << "absolute value is " << abs(c) << "\n";
     return 0;
 }
-//#include <iostream>
-//#include <gmpxx.h>
-//#include <gmp.h>
-//using std::cout;
-//int main ()
-//{
-//    mpz_class a, b, c;
-//    a = 1234;
-//    b = "-567000000000000000000000008";
-//    c = a+b;
-//    cout << "sum is " << c << "\n";
-//    cout << "absolute value is " << abs(c) << "\n";
-//    return 0;
-//}
